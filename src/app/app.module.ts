1b15c4f271ea3aec023f8655b797a6632a614200
Simeossi <simeossi@gmail.com>
2022-07-26 12:32:13 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { FavMoviesComponent } from './fav-movies/fav-movies.component';
+import { SimilarMoviesComponent } from './similar-movies/similar-movies.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MovieDetailsComponent,
+    FavMoviesComponent,
+    SimilarMoviesComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
